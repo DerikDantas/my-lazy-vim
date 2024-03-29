c901c3de2fc01b28e27e77cc14d1cad0b62d3ca9
derikdantas <derikdantas@hotmail.com>
2024-03-29 11:44:55 -0300

    my lazy vim configs

---
+++ lua/plugins/user.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "folke/trouble.nvim",
+    opts = { use_diagnostic_signs = true },
+  },
+  {
+    "xiyaowong/transparent.nvim",
+    cmd = {
+      "TransparentEnable",
+      "TransparentDisable",
+      "TransparentToggle",
+    },
+  },
+  {
+    "mg979/vim-visual-multi",
+    event = "VeryLazy",
+  },
+}
